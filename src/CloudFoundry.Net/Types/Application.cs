1c309f2d67532f56ec2e32cf454010d2c78e8677
Shantu Roy <shantu@gmail.com>
2011-09-15 17:44:50 +0000

    git-svn-id: https://tier3.svn.beanstalkapp.com/cloudfoundry/trunk/CloudFoundry.Net@79 df0ed052-c123-4f1f-bec9-fdb248c31417

---
+++ src/CloudFoundry.Net/Types/Application.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace CloudFoundry.Net.Types
+{
+    class Application : JsonBase
+    {
+        [JsonProperty(PropertyName = "name")]
+        string Name { get; set; }
+
+        [JsonProperty(PropertyName = "staging")]
+        Staging Staging { get; set; }
+
+        [JsonProperty(PropertyName = "uris")]
+        string[] Uris { get; set; }
+
+        [JsonProperty(PropertyName = "instances")]
+        int Instances { get; set; }
+
+        [JsonProperty(PropertyName = "runningInstances")]
+        int RunningInstances { get; set; }
+
+        [JsonProperty(PropertyName = "resources")]
+        Resources Resources { get; set; }
+
+        [JsonProperty(PropertyName = "state")]
+        string State { get; set; }
+
+        [JsonProperty(PropertyName = "services")]
+        string[] Services { get; set; }
+
+        [JsonProperty(PropertyName = "version")]
+        string Version { get; set; }
+
+        [JsonProperty(PropertyName = "env")]
+        Env Enviorment { get; set; }
+
+        [JsonProperty(PropertyName = "meta")]
+        AppMeta MetaData { get; set; }
+
+        public Application() {
+            Staging = new Types.Staging();
+            Resources = new Resources();
+            MetaData = new AppMeta();
+        }
+    }
+
+    internal class Staging : JsonBase 
+    {
+        [JsonProperty(PropertyName = "model")]
+        string Model { get; set; }
+
+        [JsonProperty(PropertyName = "stack")]
+        string Stack { get; set; }
+    }
+
+    internal class Resources : JsonBase
+    {
+        [JsonProperty(PropertyName = "memory")]
+        int Memory { get; set; }
+
+        [JsonProperty(PropertyName = "disk")]
+        int Disk { get; set; }
+
+        [JsonProperty(PropertyName = "fds")]
+        int Fds { get; set; }
+
+        
+
+    }
+
+    internal class Env
+    {
+        public Env () {
+
+        }
+    }
+
+    internal class AppMeta : JsonBase
+    {
+        [JsonProperty(PropertyName = "version")]
+        int Version { get; set; }
+
+        [JsonProperty(PropertyName = "created")]
+        int Created { get; set; }
+
+    }
+}
