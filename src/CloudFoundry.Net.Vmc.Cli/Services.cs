8387181a70068c5d784e265df4a6e9d2f178e9a2
Luke Bakken <luke.bakken@gmail.com>
2011-10-05 18:25:01 +0000

    "services" command.
    git-svn-id: https://tier3.svn.beanstalkapp.com/cloudfoundry/trunk/CloudFoundry.Net@153 df0ed052-c123-4f1f-bec9-fdb248c31417

--- src/CloudFoundry.Net.Vmc.Cli/Services.cs
+++ src/CloudFoundry.Net.Vmc.Cli/Services.cs
@@ -1,13 +1,38 @@
 ﻿namespace CloudFoundry.Net.Vmc.Cli
 {
+    using System;
     using System.Collections.Generic;
     using CloudFoundry.Net.Types;
 
     static partial class Program
     {
+        const string systemHeader = @"============== System Services ==============";
+        const string provisionedHeader = @"=========== Provisioned Services ============";
+
         static bool services(IList<string> unparsed)
         {
             var vc = new VcapClient();
+            IEnumerable<SystemService> systemServices = vc.GetSystemServices();
+            if (false == systemServices.IsNullOrEmpty())
+            {
+                Console.WriteLine(systemHeader);
+                foreach (SystemService s in systemServices)
+                {
+                    Console.WriteLine("{0}     {1}     {2}", s.Vendor, s.Version, s.Description);
+                }
+            }
+
+            IEnumerable<ProvisionedService> provisionedServices = vc.GetProvisionedServices();
+            if (false == provisionedServices.IsNullOrEmpty())
+            {
+                Console.WriteLine();
+                Console.WriteLine(provisionedHeader);
+                foreach (ProvisionedService s in provisionedServices)
+                {
+                    Console.WriteLine("{0}     {1}", s.Name, s.Vendor);
+                }
+            }
+
             return true;
         }
     }
