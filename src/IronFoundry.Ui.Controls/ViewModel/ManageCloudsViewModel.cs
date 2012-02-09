f38d0b41dc93cfc45cf59711a7d7c92cdab8e4bd
Luke Bakken <luke.bakken@gmail.com>
2012-02-08 17:47:41 -0800

    New screens.

---
+++ src/IronFoundry.Ui.Controls/ViewModel/ManageCloudsViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace IronFoundry.Ui.Controls.ViewModel
+{
+    using System.Collections.ObjectModel;
+    using IronFoundry.Types;
+
+    public class ManageCloudsViewModel
+    {
+        public SafeObservableCollection<CloudUrl> Clouds
+        {
+            get
+            {
+                return CloudUrl.DefaultCloudUrls;
+            }
+        }
+    }
+}
---
+++ src/IronFoundry.Ui.Controls/Views/ManageClouds.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace IronFoundry.Ui.Controls.Views
+{
+    using System.Windows;
+    using IronFoundry.Ui.Controls.ViewModel;
+
+    public partial class ManageClouds : Window
+    {
+        public ManageClouds()
+        {
+            InitializeComponent();
+            this.DataContext = new ManageCloudsViewModel();
+        }
+    }
+}
