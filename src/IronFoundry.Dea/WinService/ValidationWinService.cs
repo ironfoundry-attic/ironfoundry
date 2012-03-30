f6d01843d141752dac31a7cdfc61f3d738898d98
Luke Bakken <luke.bakken@gmail.com>
2012-03-29 17:32:36 -0700

    Added missing file, deleted test app.

---
+++ src/IronFoundry.Dea/WinService/ValidationWinService.cs
@@ -0,0 +1,47 @@
+﻿namespace IronFoundry.Dea.WinService
+{
+    using System;
+    using IronFoundry.Dea.Config;
+    using IronFoundry.Dea.Logging;
+    using IronFoundry.Dea.Properties;
+
+    [System.ComponentModel.DesignerCategory(@"Code")]
+    public class ValidationWinService : IService
+    {
+        private readonly ILog log;
+        private readonly IConfig config;
+
+        public ValidationWinService(ILog log, IConfig config)
+        {
+            this.log = log;
+            this.config = config;
+        }
+
+        public string ServiceName
+        {
+            get { return "IronFoundry.Dea.Service"; }
+        }
+
+        public ushort StartIndex
+        {
+            get { return 0; }
+        }
+
+        public StartServiceResult StartService(IntPtr serviceHandle)
+        {
+            var rv = new StartServiceResult();
+
+            if (false == config.HasAppCmd)
+            {
+                rv.Success = false;
+                string msg = Resources.ValidationWinService_AppCmdNotFound_Message;
+                log.Error(msg);
+                rv.Message = msg;
+            }
+
+            return rv;
+        }
+
+        public void StopService() { }
+    }
+}
