b8402b7e71ea83a485965bc3612b483c27bdd3c0
Luke Bakken <luke@bowbak.org>
2012-07-02 13:43:00 -0600

    Added utility to print out DEA related messages.

---
+++ tools/dea-monitor.rb
@@ -0,0 +1,29 @@
+require 'nats/client'
+
+def usage
+  puts 'Usage: nats-monitor <nats_server>
+    <nats_server> must start with nats://'
+  exit
+end
+
+$nats_server = ARGV[0]
+if $nats_server.nil? or not $nats_server.start_with?('nats://')
+  usage
+end
+
+["TERM", "INT"].each { |sig| trap(sig) { NATS.stop } }
+
+def print_message(msg, reply, sub)
+  puts "\n--------------------------------------------------\n\nSub '#{sub}' received message:\n#{msg}\n"
+end
+
+NATS.on_error { |err| puts "Server Error: #{err}"; exit! }
+
+NATS.start(:uri => $nats_server) do
+  NATS.subscribe('dea.*') { |msg, reply, sub| print_message(msg, reply, sub) }
+  NATS.subscribe('droplet.*') { |msg, reply, sub| print_message(msg, reply, sub) }
+  NATS.subscribe('router.*') { |msg, reply, sub| print_message(msg, reply, sub) }
+  NATS.subscribe('healthmanager.*') { |msg, reply, sub| print_message(msg, reply, sub) }
+end
+
+NATS.stop
