0e5a9dae1dccefeabf04bd3da1955847541ff1d6
Brent Heinz <brent.heinz@tier3.com>
2013-06-26 14:08:25 -0700

    Add setting for temp asp.net files.

--- src/IronFoundry.Warden.IISHost/Constants.cs
+++ src/IronFoundry.Warden.IISHost/Constants.cs
@@ -5,19 +5,27 @@ using System.Text;
 
 namespace IronFoundry.Warden.IISHost
 {
-    public class Constants
+    public static class Constants
     {
-        public class ConfigXPath
+        public static class ConfigXPath
         {
-            public static string ApplicationHost = "/configuration/system.applicationHost";
+            public static string Configuration = "/configuration";
+
+            public static string ApplicationHost = Configuration + "/system.applicationHost";
             public static string AppPools = ApplicationHost + "/applicationPools";
             public static string Sites = ApplicationHost + "/sites";
             public static string SiteDefaults = Sites + "/siteDefaults";
 
-            public static string WebServer = "/configuration/system.webServer";
+            public static string WebServer = Configuration + "/system.webServer";
+
+            public static class LocationSpecific
+            {
+                public static string Location = Configuration + "/location";
+                public static string SystemDotWeb = Location + "/system.web";
+            }
         }
 
-        public class FrameworkPaths
+        public static class FrameworkPaths
         {
             public static string TwoDotZero = @"%windir%\Microsoft.NET\Framework\v2.0.50727";
             public static string FourDotZero = @"%windir%\Microsoft.NET\Framework\v4.0.30319";
@@ -26,13 +34,13 @@ namespace IronFoundry.Warden.IISHost
             public static string FourDotZeroWebConfig = FourDotZero + @"\Config\web.config";
         }
 
-        public class RuntimeVersion
+        public static class RuntimeVersion
         {
             public static string VersionFourDotZero = "v4.0";
             public static string VersionTwoDotZero = "v2.0";
         }
 
-        public class PipelineMode
+        public static class PipelineMode
         {
             public static string Integrated = "Integrated";
             public static string Classic = "Classic";
